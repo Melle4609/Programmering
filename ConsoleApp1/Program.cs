b56b3cc358215e3cd45b7ab17c2e51505ee473a3
Melis Erdal <melis.erdal@elev.ga.ntig.se>
2023-08-30 10:41:35 +0200

    Luck text

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hej, Vad heter du");
+
+String name = Console.ReadLine(); 
+
+Console.WriteLine ("hej"  +  name   + " vad gillar du för mat?"); 
+
+String mat = Console.ReadLine(); 
+
+Console.WriteLine("vad är din favo låt?");
+
+String låt = Console.ReadLine();
+
+Console.WriteLine(name + mat + låt); 
+
+Console.ReadLine();
+
+
